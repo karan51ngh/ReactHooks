60544e856e2163cf4345fec701361d3d36f1d002
karan51ngh <karansingh9535@gmail.com>
2025-10-08 02:03:07 +0530

    restructuring

--- src/App.tsx
+++ src/App.tsx
@@ -1,45 +1,11 @@
 import './App.css'
-import { useState } from 'react'
-
+import UseStateUsage from './hookUsageComponents/UseStateUsage'
 
 function App() {
 
-  const [toggleText, setToggleTest] = useState<boolean>(true);
-
-  const [inputText, setinputText] = useState<string>("Hello Friend.");
-
   return (
     <div>
-      <h2>useState Hook</h2>
-
-      <h4>Example - 1</h4>
-
-      <button
-        onClick={() => {
-          setToggleTest(!toggleText)
-        }}
-      >
-        Toggle.
-      </button>
-      {toggleText && (<p>Hello Friend!</p>)}
-
-      <h4>Example - 2</h4>
-
-      <input
-        readOnly={true}
-        value={"Select Value: ".concat(inputText)}></input>
-      <select name="List"
-        onChange={(event) => {
-          setinputText(event.target.value);
-        }}
-      >
-        <option>
-          JavaScript
-        </option>
-        <option>
-          TypeScript
-        </option>
-      </select>
+      <UseStateUsage />
     </div>
   )
 }
---
+++ src/hookUsageComponents/UseStateUsage.tsx
@@ -0,0 +1,42 @@
+import { useState } from 'react';
+
+
+export default function UseStateUsage() {
+    const [toggleText, setToggleTest] = useState<boolean>(true);
+    const [inputText, setinputText] = useState<string>("Hello Friend.");
+    return (
+        <div>
+            <h2>useState Hook</h2>
+
+            <h4>Example - 1</h4>
+
+            <button
+                onClick={() => {
+                    setToggleTest(!toggleText)
+                }}
+            >
+                Toggle.
+            </button>
+            {toggleText && (<p>Hello Friend!</p>)}
+
+            <h4>Example - 2</h4>
+
+            <input
+                readOnly={true}
+                value={"Select Value: ".concat(inputText)}></input>
+            <select name="List"
+                onChange={(event) => {
+                    setinputText(event.target.value);
+                }}
+            >
+                <option>
+                    JavaScript
+                </option>
+                <option>
+                    TypeScript
+                </option>
+            </select>
+
+        </div>
+    );
+}
